69b87cf6dbdef5a6bfba25ed3e0570ec1697e10d
VastView <johanreynekemail@gmail.com>
2023-04-25 11:19:06 +1200

    Add testing.gd
    The Testing singleton.
    
    For the user to debug their code with the `Testing.ensure()` method (the plugin's version of `assert`).
    
    Also caches errors generated by user code to be collected and displayed by plugin when testing.

--- Simple Testing for Godot/addons/simple testing/simple_testing.gd
+++ Simple Testing for Godot/addons/simple testing/simple_testing.gd
@@ -3,11 +3,18 @@ extends EditorPlugin
 
 var dock: Control
 
+# When the plugin is added/activated:
 func _enter_tree():
+	# Load the "Testing" singleton:
+	add_autoload_singleton("Testing", "res://addons/simple testing/testing.gd")
+	
+	# Load editor dock:
 	dock = load("res://addons/simple testing/ui/dock.tscn").instantiate()
 	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UR, dock)
 
-
+# When the plugin is removed/deactivated:
 func _exit_tree():
+	# Clean up assets:
+	remove_autoload_singleton("Testing")
 	remove_control_from_docks(dock)
 	dock.queue_free()
