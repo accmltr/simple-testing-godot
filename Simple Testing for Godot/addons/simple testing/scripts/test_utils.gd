86d8d2e637e585d6003e8e7d0ee0265f9f28e1e6
VastView <johanreynekemail@gmail.com>
2023-04-26 20:38:24 +1200

    Add TestUtils

---
+++ Simple Testing for Godot/addons/simple testing/scripts/test_utils.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func float_equals(value1: float, value2: float, epsilon: float) -> bool:
+	return abs(value1 - value2) <= epsilon
--- Simple Testing for Godot/addons/simple testing/simple_testing.gd
+++ Simple Testing for Godot/addons/simple testing/simple_testing.gd
@@ -7,6 +7,7 @@ var dock: Control
 func _enter_tree():
 	# Load the "Testing" singleton:
 	add_autoload_singleton("Testing", "res://addons/simple testing/scripts/testing.gd")
+	add_autoload_singleton("TestUtils", "res://addons/simple testing/scripts/test_utils.gd")
 	
 	# Load editor dock:
 	dock = load("res://addons/simple testing/dock/dock.tscn").instantiate()
@@ -17,5 +18,6 @@ func _enter_tree():
 func _exit_tree():
 	# Clean up assets:
 	remove_autoload_singleton("Testing")
+	remove_autoload_singleton("TestUtils")
 	remove_control_from_docks(dock)
 	dock.queue_free()
