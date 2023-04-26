5959271caf028704416e4e2d4b8c7394ccbd9775
VastView <johanreynekemail@gmail.com>
2023-04-27 11:32:32 +1200

    Add test that uses Testing.error_happens()

--- Simple Testing for Godot/sample tests/my_tests.gd
+++ Simple Testing for Godot/sample tests/my_tests.gd
@@ -4,3 +4,7 @@ func test_plus():
 	istrue(1 + 1 == 2, self, "1 + 1 did not equal 3")
 	istrue(1 + 2 == 3, self, "1 + 2 did not equal 3", 3)
 
+func test_error_happens():
+	var error_prone_code = func() :
+		istrue(0 == 10, self, "0 did not equal 10")
+	error_happens(error_prone_code, self, "The error prone code did not have any errors :(")
