3f35a63ab7ca3fd5c4b7d09c8e2b35d7026d7d0a
VastView <johanreynekemail@gmail.com>
2023-04-26 20:27:03 +1200

    Now shows errors under test method tree items

--- Simple Testing for Godot/sample tests/my_tests.gd
+++ Simple Testing for Godot/sample tests/my_tests.gd
@@ -1,5 +1,6 @@
 extends UnitTest
 
 func test_plus():
-	istrue(1 + 1 == 3, self, "1 + 1 did not equal 3")
-	istrue(1 + 2 == 3, self, "1 + 2 did not equal 3")
+	istrue(1 + 1 == 2, self, "1 + 1 did not equal 3")
+	istrue(1 + 2 == 3, self, "1 + 2 did not equal 3", 3)
+
