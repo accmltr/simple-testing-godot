8f6f222375e21db2c01486c8d2d8171529034c81
VastView <johanreynekemail@gmail.com>
2023-05-05 21:45:09 +1200

    Make `epsilon` optional in `float_equals`, `test_utils.gd`

--- scripts/test_utils.gd
+++ scripts/test_utils.gd
@@ -1,5 +1,5 @@
 @tool
 extends Node
 
-func float_equals(value1: float, value2: float, epsilon: float) -> bool:
+func float_equals(value1: float, value2: float, epsilon: float = 0.0001) -> bool:
 	return abs(value1 - value2) <= epsilon
